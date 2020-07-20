da1660485c79fe391b18dfad5f79d0ae96dfad44
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-07-20 21:38:53 +0100

    Initial Commit

---
+++ GAM730-TestingFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GAM730-TestingFirebase
+//
+//  Created by Rachel Saunders on 20/07/2020.
+//  Copyright © 2020 Rachel Saunders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
